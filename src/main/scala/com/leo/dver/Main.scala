facdd8c04cec8bf7b41fde71b110f5c40d75e785
Lew <barpas@zoho.com>
2018-09-26 18:52:37 +0200

    allow text file editing

--- src/main/scala/com/leo/dver/Main.scala
+++ src/main/scala/com/leo/dver/Main.scala
@@ -1,6 +1,6 @@
 package me.leo.dver
 
-import java.io.{InputStream, OutputStream, File}
+import java.io.{InputStream, OutputStream, File, PrintWriter}
 import java.net.InetSocketAddress
 import scala.io.Source._
 
@@ -10,14 +10,15 @@ object Main {
 
 	def main(args: Array[String]) {
 		val server = HttpServer.create(new InetSocketAddress(8000), 0)
-		server.createContext("/", new RootHandler())
+		server.createContext("/r/", new ReadHandler())
+		server.createContext("/w/", new WriteHandler())
 		server.setExecutor(null)
 		server.start()
 	}
 
 }
 
-class RootHandler extends HttpHandler {
+class ReadHandler extends HttpHandler {
 	def tag(name:String, content:String) : String =
 		"<" + name + ">" + content + "</" + name + ">"
 	def tag(name:String, params:String, content:String) : String =
@@ -32,28 +33,81 @@ class RootHandler extends HttpHandler {
 		tag("a", "href=\"" + url + "\"", text)
 
 	def fileLink(f:File) : String =
-		simpleLink("^\\.".r.replaceFirstIn(f.getPath,""), f.getName)		
+		simpleLink("/r/" + f.getPath, f.getName)		
+
+	def encoding (v:String) = "<meta charset=\"" + v + "\"/>"
+	def styles = tag("style", "textarea{ width:90%; height:90%; }")
+	
+	def head = tag("head", encoding("utf-8") + styles)
 
 	def doc(innerHtml:String) : String = tag("html",
-		tag("body", innerHtml)
+		head + tag("body", innerHtml)
 	)
 
-	def plainText(file:File) : String =
-		fromFile(file).mkString
-			.replaceAll("<", "&lt;")
-			.replaceAll(">", "&gt;")
+	def jsFun(name:String, body:String) =
+		tag("script", "type=\"text/javascript\"",
+			"function " + name + "() {" + body + "}"
+		)
+
+	def jsHttpPost(uri:String, body:String) =
+		"var o = new XMLHttpRequest();" +
+		"o.open(\"POST\", \"" + uri + "\", true);" +
+		"o.onreadystatechange = function(){ " +
+			"if(this.readyState == XMLHttpRequest.DONE" +
+			" && this.status == 200) alert(\"done!\");};" +
+		"o.send("+body+");"
+	
+	def jsId(id:String) = "document.getElementById(\"" + id + "\")"
+
+	def textEditor(file:File) : String =
+		tag("textarea", "id=\"texted\"", fromFile(file).mkString) +
+		jsFun("updateFile",
+			jsHttpPost("/w/" + file.getPath,
+				jsId("texted") + ".value"
+			)
+		) + "<br/>" +
+		tag("button", "onclick=\"updateFile()\"", "save")
 
 	def doc(file:File) : String = file.isDirectory match {
 		case true => doc(blist(file.listFiles.toList.map(fileLink)))
-		case false => doc(tag("pre", plainText(file)))
+		case false => doc(textEditor(file))
+	}
+
+	def lPath(uri:String) = uri match {
+		case "/r/" => "."
+		case _ => "^/r/".r.replaceFirstIn(uri,"")
 	}
 
 	def handle(t: HttpExchange) {
-		val response = doc(new File( "." + t.getRequestURI.getPath))
+		val response = doc(new File(lPath(t.getRequestURI.getPath)))
 		t.sendResponseHeaders(200, response.length())
 		val os = t.getResponseBody
 		os.write(response.getBytes)
 		os.close()
 	}
 
+}
+
+class WriteHandler extends HttpHandler {
+	def lPath(t:HttpExchange) =
+		"." + "^/w".r.replaceFirstIn(t.getRequestURI.getPath,"")
+
+	def handle(t:HttpExchange) {
+		{
+			val o = new PrintWriter(lPath(t))
+				val i = t.getRequestBody
+
+				Iterator.continually(i.read)
+				.takeWhile(-1 !=)
+				.foreach(o.write)
+				o.close
+		}
+
+		val response = "<resp status=\"ok\"/>"
+		t.sendResponseHeaders(200, response.length)
+		
+		val o = t.getResponseBody
+		o.write(response.getBytes);
+		o.close
+	}
 }
