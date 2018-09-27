831dc3d0450175874faf57af0471e4966880ab49
Lew <barpas@zoho.com>
2018-09-28 01:37:49 +0200

    refactor:tag clss

--- src/main/scala/com/leo/dver/Main.scala
+++ src/main/scala/com/leo/dver/Main.scala
@@ -3,6 +3,7 @@ package me.leo.dver
 import java.io.{InputStream, OutputStream, File, PrintWriter}
 import java.net.InetSocketAddress
 import scala.io.Source._
+import scala.collection.immutable.Map
 
 import com.sun.net.httpserver.{HttpExchange, HttpHandler, HttpServer}
 
@@ -18,36 +19,71 @@ object Main {
 
 }
 
-class ReadHandler extends HttpHandler {
-	def tag(name:String, content:String) : String =
-		"<" + name + ">" + content + "</" + name + ">"
-	def tag(name:String, params:String, content:String) : String =
-		"<" + name + " " + params + ">" + content + "</" + name + ">"
+class Tag(name:String, args:Map[String,String], value:Option[String]) {
 
-	def blist(items : List[String]) : String =
-		"<ul>" + items.foldLeft("") { (x, r) =>
-			x + "<li>" + r + "</li>"
-		} + "</ul>"
-
-	def simpleLink(url:String, text:String) :String =
-		tag("a", "href=\"" + url + "\"", text)
-
-	def fileLink(f:File) : String =
-		simpleLink("/r/" + f.getPath, f.getName)		
-
-	def encoding (v:String) = "<meta charset=\"" + v + "\"/>"
-	def styles = tag("style", "textarea{ width:90%; height:90%; }")
-	
-	def head = tag("head", encoding("utf-8") + styles)
-
-	def doc(innerHtml:String) : String = tag("html",
-		head + tag("body", innerHtml)
+	def this(name:String) = this(name, Map(), None)
+	def this(name:String, args:Map[String,String]) =
+		this(name, args, None)
+	def this(name:String, value:String) = this(name, Map(), Some(value))
+	def this(name:String, children:List[Tag]) = this(name, Map(),
+		Some(children.foldLeft(""){(a, v)=> a + v.toString})
 	)
 
-	def jsFun(name:String, body:String) =
-		tag("script", "type=\"text/javascript\"",
-			"function " + name + "() {" + body + "}"
-		)
+	private def argsStr = args.size match {
+		case 0 => ""
+		case _ => " " + args.keys.fold ("") {
+			(acc, v) => acc + v + "=\"" + args(v) +	"\" "
+		}
+	}
+
+	private def closingStr = value match {
+		case Some(s) => ">" + s + "</" + name + ">"
+		case None => "/>"
+	}
+
+	override def toString =
+		"<" + name + argsStr + closingStr
+}
+
+class BList(items:List[String]) extends Tag("ul",
+	items.foldLeft("") { (x, r) =>
+		x + "<li>" + r + "</li>"
+	}
+)
+
+class Link(url:String, text:String) extends Tag(
+	"a",Map("href" -> url), Some(text)
+)
+
+class Encoding(v:String) extends Tag("meta", Map("charset" -> v))
+
+class CssBlock(rules:Map[String,String]) extends Tag("style", Map(),
+	Some(rules.keys.foldLeft("") {
+		(a, k) => a + "\n" + k + "{" + rules(k) + "}"
+	}
+))
+
+class JsFunction(name:String, body:String) extends Tag(
+	"script", Map("type" -> "text/javascript"),
+	Some("function " + name + "() {" + body + "}")
+)
+
+class Button(text:String, action:String) extends Tag(
+	"button", Map("onclick"->action), Some(text)
+)
+
+class ReadHandler extends HttpHandler {
+	def fileLink(f:File) : String =
+		new Link("/r/" + f.getPath, f.getName).toString
+
+	def headCss(styles:Map[String,String]) = new Tag("head",
+		List(new Encoding("utf-8"), new CssBlock(styles))
+	)
+
+	def doc(body:List[Tag]) : String = new Tag("html",
+		List(	headCss(Map("textarea"->"width:90%;height:90%")),
+			new Tag("body", body)
+		)).toString
 
 	def jsHttpPost(uri:String, body:String) =
 		"var o = new XMLHttpRequest();" +
@@ -64,34 +100,33 @@ class ReadHandler extends HttpHandler {
 		case false => ""
 	}
 
-	def textEditor(file:File) : String =
-		tag("h3", "File: " + file.getPath) +
-		tag("textarea", "id=\"texted\"", fileContent(file)) +
-		jsFun("updateFile",
+	def textEditor(file:File) = List(
+		new Tag("h3", Map(), Some("File: " + file.getPath)),
+		new Tag("textarea", Map("id" -> "texted"),
+			Some(fileContent(file))),
+		new JsFunction("updateFile",
 			jsHttpPost("/w/" + file.getPath,
 				jsId("texted") + ".value"
-			)
-		) + "<br/>" +
-		tag("button", "onclick=\"updateFile()\"", "save")
+			)),
+		new Tag("br"),
+		new Button("save", "updateFile()")
+	)
 
-	def textInput(id:String) =
-		"<input type=\"text\" id=\"" + id + "\"/>"
+	def dirList(f:File) = List(
+		new Tag("h3", Map(), Some("Directory: " + f.getPath)),
+		new BList(f.listFiles.toList.map(fileLink)),
+		new Tag("br"),
+		new Tag("input", Map("type"->"text", "id"->"newfilename")),
+		new JsFunction("newFile",
+			"window.location.href=\"/r/" +
+				f.getPath + "/\" + " +
+				jsId("newfilename") + ".value;"
+		), 
+		new Button("Create", "newFile()")
+	)
 
-	def dirList(f:File) =
-		doc(
-			tag("h3", "Directory: " + f.getPath) +
-			blist(f.listFiles.toList.map(fileLink)) + "<br/>" +
-			textInput("newfilename") + 
-			jsFun("newFile",
-				"window.location.href=\"/r/" +
-					f.getPath + "/\" + " +
-					jsId("newfilename") + ".value;"
-			) + 
-			tag("button", "onclick=\"newFile()\"", "create")
-		)
-
-	def doc(file:File) : String = file.isDirectory match {
-		case true => dirList(file) 
+	def fileDoc(file:File) : String = file.isDirectory match {
+		case true => doc(dirList(file))
 		case false => doc(textEditor(file))
 	}
 
@@ -101,7 +136,7 @@ class ReadHandler extends HttpHandler {
 	}
 
 	def handle(t: HttpExchange) {
-		val response = doc(new File(lPath(t.getRequestURI.getPath)))
+		val response = fileDoc(new File(lPath(t.getRequestURI.getPath)))
 		t.sendResponseHeaders(200, response.length())
 		val os = t.getResponseBody
 		os.write(response.getBytes)
